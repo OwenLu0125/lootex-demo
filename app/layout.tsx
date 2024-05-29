3c5f52b3d9b65adfe96e29ed86b382cffe71a1d7
OwenLu <gg0125gg@gmail.com>
2024-05-29 21:50:36 +0800

    chore: Update RootLayout with lazy loading for images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,21 @@
+import { Box, Skeleton } from '@mui/material';
+import { Suspense } from 'react';
 import '../styles/globals.css';
 
 import Providers from 'components/providers';
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <Providers>{children}</Providers>
+        <Suspense fallback={<div>
+          <Box sx={{ width: 300 }}>
+            <Skeleton />
+            <Skeleton animation="wave" />
+            <Skeleton animation={false} />
+          </Box></div>}>
+          <Providers>{children}</Providers>
+        </Suspense>
       </body>
     </html>
   );
