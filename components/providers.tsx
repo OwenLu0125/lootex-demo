0f465bd526f6d0350d43ca4e7d75bb71ee7ccc49
OwenLu <gg0125gg@gmail.com>
2024-05-29 13:08:58 +0800

    chore: update dependencies and providers file formatting

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {http} from 'viem';
-import {mainnet, sepolia} from 'viem/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { http } from 'viem';
+import { mainnet, sepolia } from 'viem/chains';
 
-import type {PrivyClientConfig} from '@privy-io/react-auth';
-import {PrivyProvider} from '@privy-io/react-auth';
-import {WagmiProvider, createConfig} from '@privy-io/wagmi';
+import type { PrivyClientConfig } from '@privy-io/react-auth';
+import { PrivyProvider } from '@privy-io/react-auth';
+import { WagmiProvider, createConfig } from '@privy-io/wagmi';
 
 const queryClient = new QueryClient();
 
@@ -24,13 +24,13 @@ const privyConfig: PrivyClientConfig = {
     requireUserPasswordOnCreate: true,
     noPromptOnSignature: false,
   },
-  loginMethods: ['wallet', 'email', 'sms'],
+  loginMethods: ['email', 'google', 'discord'],
   appearance: {
     showWalletLoginFirst: true,
   },
 };
 
-export default function Providers({children}: {children: React.ReactNode}) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <PrivyProvider
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
