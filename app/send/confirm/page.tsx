83af78220433aa550e2a322a889ae1badf53080a
OwenLu <gg0125gg@gmail.com>
2024-05-29 21:40:00 +0800

    feat: Add lazy loading for images in Confirm page

--- app/send/confirm/page.tsx
+++ app/send/confirm/page.tsx
@@ -1,8 +1,10 @@
 'use client';
 import Link from 'next/link'
+import { Suspense } from 'react';
 import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 import { useAccount, useBalance } from 'wagmi';
+import { Box, Skeleton } from '@mui/material';
 
 
 export default function Confirm() {
@@ -18,41 +20,49 @@ export default function Confirm() {
   }, [searchParams])
 
   return (
-    <div className="flex flex-col h-screen p-5">
-      <header className="p-4">
-        <Link href="/">
-          <button className="absolute top-0 left-0 m-4">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-            </svg>
-          </button>
-        </Link>
-      </header>
-      <h1 className="text-lg font-bold">How much do you want to send?</h1>
+    <Suspense fallback={<div>
+      <Box sx={{ width: 300 }}>
+        <Skeleton />
+        <Skeleton animation="wave" />
+        <Skeleton animation={false} />
+      </Box></div>}>
+      <div className="flex flex-col h-screen p-5">
+        <header className="p-4">
+          <Link href="/send">
+            {/* TODO: change icons */}
+            <button className="absolute top-0 left-0 m-4">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+              </svg>
+            </button>
+          </Link>
+        </header>
+        <h1 className="text-lg font-bold">How much do you want to send?</h1>
 
-      <main className="flex-1 p-4 overflow-auto">
-        <section className="mb-4 p-4 bg-white shadow-md rounded-md">
-          <h2 className="text-lg font-bold">資產:</h2>
-          {data && (
-            <p className="text-sm text-gray-500">餘額 :{data?.formatted} {data?.symbol}</p>
-          )}
-        </section>
+        <main className="flex-1 p-4 overflow-auto">
+          <section className="mb-4 p-4 bg-white shadow-md rounded-md">
+            <h2 className="text-lg font-bold">資產:</h2>
+            {data && (
+              <p className="text-sm text-gray-500">餘額 :{data?.formatted} {data?.symbol}</p>
+            )}
+          </section>
 
-        <section className="mb-4 p-4 bg-white shadow-md rounded-md">
-          <h2 className="text-lg font-bold">TO:</h2>
-          <p className="text-sm text-gray-500">{transactionAddress}</p>
-        </section>
+          <section className="mb-4 p-4 bg-white shadow-md rounded-md">
+            <h2 className="text-lg font-bold">TO:</h2>
+            <p className="text-sm text-gray-500">{transactionAddress}</p>
+          </section>
 
-        <section className="mb-4 p-4 bg-white shadow-md rounded-md">
-          <h2 className="text-lg font-bold">Estimated fee</h2>
-          <p className="text-sm text-gray-500">Morket -30 sec</p>
-          <p className="text-sm text-gray-500">Max fee: 0.00101457 SepoliaETH</p>
-        </section>
-      </main>
+          <section className="mb-4 p-4 bg-white shadow-md rounded-md">
+            <h2 className="text-lg font-bold">Estimated fee</h2>
+            <p className="text-sm text-gray-500">Morket -30 sec</p>
+            <p className="text-sm text-gray-500">Max fee: 0.00101457 SepoliaETH</p>
+          </section>
+        </main>
 
-      <footer className="p-4 bg-white shadow-md">
-        <button className="w-full py-2 px-4 bg-blue-600 text-white rounded-md">下一頁</button>
-      </footer>
-    </div>
+        <footer className="p-4 bg-white shadow-md">
+          <button className="w-full py-2 px-4 bg-blue-600 text-white rounded-md">下一頁</button>
+        </footer>
+      </div>
+    </Suspense>
   )
 }
